2ae3153717c8385ad68701113ee451a143ad519d
Eric Fiorani <efiorani305@gmail.com>
2018-01-30 15:35:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+var name = "Brandy"
+var holiday = "Valentines Day"
+var days = 16
+
+function happyHolidays(){
+  return "Happy Holidays!"
+}
+
+function happyHolidaysTo(){
+  return `Happy Holidays, ${name}!`
+}
+
+function happyHolidayTo() {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown() {
+  return `It's ${days} days until ${holiday}!`
+}
